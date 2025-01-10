54e8dee506537b3b687b8cf20d626eafee4d33c4
Rushikesh Yemul <yemulrushikesh56@gmail.com>
2025-01-10 13:14:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import { Todo } from "./projects/Todo/Todo";
+
+const App = () => {
+  return (
+    <>
+      <Todo />
+    </>
+  );
+};
+
+export default App;
